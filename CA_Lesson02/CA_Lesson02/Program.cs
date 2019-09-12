3ab39765b9497415d334eb5593369082e710f8fc
Loki <DadPoolTV@gmail.com>
2019-09-12 17:52:48 -0600

    CA_Lesson03 Completed

--- CA_Lesson02/CA_Lesson02/Program.cs
+++ CA_Lesson02/CA_Lesson02/Program.cs
@@ -19,6 +19,29 @@ namespace CA_Lesson02
             Console.WriteLine(age);
             Console.WriteLine(weight);
             Console.WriteLine(spayed);
+
+            //Impicit and Explicit Conversion
+
+            int faveNumber = 0;
+
+            Console.Write("Please enter your favorite number: ");
+
+            // Turn that answer into an int
+
+            // Attempt 1: use implicit conversion fails
+            // faveNumber = Console.ReadLine();
+
+            // Attempt 2: use explicit conversion fails 
+            // faveNumber = (int)Console.ReadLine();
+
+            // Attempt 3: use Convert method is the only way to achieve what we want here, NOTE if the user enters anything but a whole number, the entire project will crash
+            faveNumber = Convert.ToInt32(Console.ReadLine()); 
+
+            //Output without the $ in front (seen on line 43) results the text showiwing up as it is listed in the statement
+            Console.WriteLine("The number you have entered is {faveNumber}");
+            //Output with the $ in front (as seen on line 43) results with the text showing with the number the user has entered previously on line 38
+            Console.WriteLine($"The number you have entered is {faveNumber}");
+
         }
     }
 }
